9304539bb71a3d3bf832da89f1f4855d2e092949
Mohammad <mhdbabakhani14@gmail.com>
2025-09-24 23:42:05 +0330

    sixth try to fix server json

--- api/server.js
+++ api/server.js
@@ -1,3 +1,5 @@
+// api/server.js
+
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("api/db.json");
@@ -6,8 +8,7 @@ const middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use(router);
 
+// برای استفاده در Vercel باید از این مدل تابع استفاده کنید
 module.exports = (req, res) => {
-  return new Promise((resolve) => {
-    server(req, res, resolve);
-  });
+  server(req, res);
 };
