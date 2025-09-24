97994dd0e744a1c2f48b5489d0d2c0b4ecdddd1c
Mohammad <mhdbabakhani14@gmail.com>
2025-09-24 21:46:47 +0330

    add example card to media section for demo purposes

--- src/components/Media.jsx
+++ src/components/Media.jsx
@@ -1,10 +1,28 @@
-import { Typography } from "@mui/material";
+import { Button, Card, CardActions, CardContent, CardMedia, Typography } from "@mui/material";
 
 const ComponentName = () => {
     return (
-    <Typography>
-      فیلم و سریال
-    </Typography>
+      <Card sx={{ maxWidth: 345 }}>
+        <CardMedia
+          component="img"
+          alt="green iguana"
+          height="140"
+          image="https://placehold.co/600x400"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Lizard
+          </Typography>
+          <Typography variant="body2" sx={{ color: "text.secondary" }}>
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Share</Button>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
     );
 };
 
