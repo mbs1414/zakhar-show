db7d0f287cf854abc47d0278668a75711cdf2fad
Mohammad <mhdbabakhani14@gmail.com>
2025-09-28 23:31:48 +0330

    improving responsive styles

--- src/components/MediaCard.jsx
+++ src/components/MediaCard.jsx
@@ -9,8 +9,10 @@ import {
   Typography,
 } from "@mui/material";
 import Ratings from "./Ratings";
+import { useMediaQueries } from "../utils/mediaQueries";
 
 const MediaCard = () => {
+  const { isSm } = useMediaQueries();
   const movie = {
     id: 14,
     title: "John Henry",
@@ -34,93 +36,118 @@ const MediaCard = () => {
     rating: 0,
   };
   return (
-    <Card
-      sx={{
-        backgroundColor: (theme) => theme.palette.background.paper,
-        display: "flex",
-        width: "100%",
-      }}
+    <Box
+      sx={{ ...(!isSm && { mt: "10rem", pt: "2rem", position: "relative" }) }}
     >
-      <CardMedia
-        component="img"
-        image={movie.thumbnail}
-        alt="Paella dish"
-        sx={{ width: "15rem", height: "23rem", objectFit: "cover" }}
-      />
-      <CardContent sx={{ width: "60%", "&.MuiCardContent-root": { pb: 0 } }}>
-        <Typography variant="h5" component="div">
-          {movie.title}
-        </Typography>
-        <Box sx={{ display: "flex", gap: "0.5rem" }}>
-          <Typography variant="h6" component="div">
-            genres:
-          </Typography>
-          <Stack
-            direction="row"
-            sx={{ display: "flex", gap: "0.5rem", flexWrap: "wrap" }}
-          >
-            {movie.genres.map((genre) => (
-              <Chip
-                key={genre}
-                label={genre}
-                color="primary"
-                sx={{
-                  color: "primary.contrastText",
-                }}
-              />
-            ))}
-          </Stack>
-        </Box>
-        <Box component="div">
-          <Typography variant="h6" component="div">
-            description:
-          </Typography>
-          <Tooltip title={movie.extract}>
-            <Typography
-              sx={{
-                display: "-webkit-box",
-                WebkitBoxOrient: "vertical",
-                WebkitLineClamp: 3,
-                overflow: "hidden",
-                textOverflow: "ellipsis",
-              }}
-            >
-              {movie.extract}
-            </Typography>
-          </Tooltip>
-        </Box>
-        <Box component="div" sx={{ display: "flex", gap: "0.5rem" }}>
-          <Typography variant="h6" component="div">
-            cast:
-          </Typography>
-          <Stack
-            direction="row"
-            sx={{ display: "flex", gap: "0.5rem", flexWrap: "wrap" }}
-          >
-            {movie.cast.map((actor) => (
-              <Chip
-                variant="outlined"
-                key={actor}
-                label={actor}
-                color="info"
-                sx={{
-                  color: "text.primary",
-                }}
-              />
-            ))}
-          </Stack>
-        </Box>
-        <Box
-          component="div"
-          sx={{ display: "flex", gap: "0.5rem", alignItems: "center" }}
+      <Card
+        sx={{
+          backgroundColor: (theme) => theme.palette.background.paper,
+          display: "flex",
+          flexDirection: isSm ? "row" : "column",
+          width: "100%",
+          // ...(!isSm && { position: "relative" }),
+        }}
+      >
+        <CardMedia
+          component="img"
+          image={movie.thumbnail}
+          alt="Paella dish"
+          sx={{
+            width: "15rem",
+            height: isSm ? "23rem" : "21rem",
+            objectFit: "fill",
+            flex: '1 0 auto',
+            m: "auto",
+            ...(!isSm && {
+              position: "absolute",
+              top: "80%%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+              borderRadius: "1rem"
+            }),
+          }}
+        />
+        <CardContent
+          sx={{
+            ...(!isSm && { mt: "11rem" }),
+            width: isSm ? "60%" : "90%",
+            "&.MuiCardContent-root": { pb: 0 },
+          }}
         >
-          <Typography variant="h6" component="div">
-            rating:
+          <Typography variant="h5" component="div">
+            {movie.title}
           </Typography>
-          <Ratings />
-        </Box>
-      </CardContent>
-    </Card>
+          <Box sx={{ display: "flex", gap: "0.5rem" }}>
+            <Typography variant="h6" component="div">
+              genres:
+            </Typography>
+            <Stack
+              direction="row"
+              sx={{ display: "flex", gap: "0.5rem", flexWrap: "wrap" }}
+            >
+              {movie.genres.map((genre) => (
+                <Chip
+                  key={genre}
+                  label={genre}
+                  color="primary"
+                  sx={{
+                    color: "primary.contrastText",
+                  }}
+                />
+              ))}
+            </Stack>
+          </Box>
+          <Box component="div">
+            <Typography variant="h6" component="div">
+              description:
+            </Typography>
+            <Tooltip title={movie.extract}>
+              <Typography
+                sx={{
+                  display: "-webkit-box",
+                  WebkitBoxOrient: "vertical",
+                  WebkitLineClamp: 3,
+                  overflow: "hidden",
+                  textOverflow: "ellipsis",
+                }}
+              >
+                {movie.extract}
+              </Typography>
+            </Tooltip>
+          </Box>
+          <Box component="div" sx={{ display: "flex", gap: "0.5rem" }}>
+            <Typography variant="h6" component="div">
+              cast:
+            </Typography>
+            <Stack
+              direction="row"
+              sx={{ display: "flex", gap: "0.5rem", flexWrap: "wrap" }}
+            >
+              {movie.cast.map((actor) => (
+                <Chip
+                  variant="outlined"
+                  key={actor}
+                  label={actor}
+                  color="info"
+                  sx={{
+                    color: "text.primary",
+                  }}
+                />
+              ))}
+            </Stack>
+          </Box>
+          <Box
+            component="div"
+            sx={{ display: "flex", gap: "0.5rem", alignItems: "center" }}
+          >
+            <Typography variant="h6" component="div">
+              rating:
+            </Typography>
+            <Ratings />
+          </Box>
+        </CardContent>
+      </Card>
+    </Box>
   );
 };
 
