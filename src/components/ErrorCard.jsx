c5a1e1b0ee1450ddb2cac67014997a28cea25ab8
Mohammad <mhdbabakhani14@gmail.com>
2025-10-02 20:33:15 +0330

    improve styling of ErrorCard component

--- src/components/ErrorCard.jsx
+++ src/components/ErrorCard.jsx
@@ -2,19 +2,18 @@ import { Box, Card, Typography } from "@mui/material";
 
 const ErrorCard = ({ textError = "زاخار این جا پیدات نشه" }) => {
   return (
-    <Box width="100vh" sx={{ m: "auto" }}>
-      <Card variant="elevation">
-        <Typography
-          fontFamily="tanha"
-          sx={{
-            textAlign: "center",
-            color: "red",
-            fontWeight: "bold"
-          }}
-        >
-          {textError}
-        </Typography>
-      </Card>
+    <Box sx={{ minHeight: "100vh", backgroundColor: "#282A36", display: "flex", justifyContent: "center", alignItems: "center" }}>
+      <Typography
+        fontFamily="tanha"
+        variant="h5"
+        sx={{
+          textAlign: "center",
+          color: "red",
+          fontWeight: "bold",
+        }}
+      >
+        {textError}
+      </Typography>
     </Box>
   );
 };
